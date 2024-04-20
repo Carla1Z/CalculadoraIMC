faca2342b0032830f179ee5ad79b5bc6c746b470
Carla1Z <carla_sabrina_zarate@hotmail.com>
2024-04-20 17:36:18 -0300

    calculo de resultado

--- MVVM/Models/IMC.cs
+++ MVVM/Models/IMC.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,20 @@ using System.Threading.Tasks;
 
 namespace CalculadoraIMC.MVVM.Models
 {
+    [AddINotifyPropertyChangedInterface]
     public class IMC
-    { 
+    {
+        private float result;
+
         public float Height { get; set; }
         public float Weight { get; set; }
-        public float Result { get; set; }
+        public float Result
+        {
+            get 
+            {
+                return ((Weight / Height) / Height) * 10000;
+            }
+        }
 
     }
 }
